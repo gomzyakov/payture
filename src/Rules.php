776e8f1273af83c989c95b177babc8c253538449
gomzyakov <alexander.gomzyakov@gmail.com>
2022-10-01 16:26:44 +0500

    Draft of Payture InPay Client

---
+++ src/Rules.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Gomzyakov\CS;
+
+use function json_decode;
+
+/**
+ * The class containing the rules for `php-cs-fixer`.
+ *
+ * @see https://github.com/FriendsOfPHP/PHP-CS-Fixer
+ * @see https://mlocati.github.io/php-cs-fixer-configurator/
+ */
+class Rules
+{
+    /**
+     * @param array<string, mixed> $overwritten_rules
+     *
+     * @return array<string, mixed>
+     */
+    public static function getRules(array $overwritten_rules = []): array
+    {
+        /** @var string $pint_rules */
+        $pint_rules = file_get_contents(__DIR__ . '/../pint.json');
+
+        /** @var array{rules: array<mixed>} $rules */
+        $rules = json_decode($pint_rules, true);
+
+        return array_merge($rules['rules'], $overwritten_rules);
+    }
+}
