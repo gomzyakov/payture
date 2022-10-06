a7b17a73c99946402ae529fdef1bf067496ce937
Shirokov Nickolai <43547352+shirokovnv@users.noreply.github.com>
2022-10-06 07:04:15 +0500

    Add API links in annotation for the PaytureOperation (#23)

--- src/PaytureOperation.php
+++ src/PaytureOperation.php
@@ -5,25 +5,42 @@ namespace Gomzyakov\Payture;
 /**
  * Operations that payture accepts.
  *
- * TODO Move to References
- * TODO Link
+ * @see https://payture.com/en/api/#inpay_
  */
 enum PaytureOperation
 {
     /**
-     * TODO Add description.
+     * @see https://payture.com/en/api/#inpay_init_
      */
     case Init;
 
+    /**
+     * @see https://payture.com/en/api/#inpay_pay_
+     */
     case Pay;
 
+    /**
+     * @see https://payture.com/en/api/#inpay_charge_
+     */
     case Charge;
 
+    /**
+     * @see https://payture.com/en/api/#inpay_unblock_
+     */
     case Unblock;
 
+    /**
+     * @see https://payture.com/en/api/#inpay_refund_
+     */
     case Refund;
 
+    /**
+     * @see https://payture.com/en/api/#inpay_getstate_
+     */
     case PayStatus;
 
+    /**
+     * @see https://payture.com/en/api/#inpay_paystatus_
+     */
     case GetState;
 }
